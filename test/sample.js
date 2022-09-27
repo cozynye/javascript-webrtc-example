08db23aa0e90724b7560e597b768aedfc214243a
cozynye <cozynye@gmail.com>
2022-09-27 11:13:50 +0900

    add:test 페이지 추가

--- test/sample.js
+++ test/sample.js
@@ -1,75 +1,166 @@
-// 버튼 객체
+// Button DOM
+const createSessionBtn = document.getElementById("createSessionBtn");
 const createRoomBtn = document.getElementById("createRoomBtn");
-const joinRoomBtn = document.getElementById("joinRoomBtn");
+const roomListBtn = document.getElementById("roomListBtn");
 const publishBtn = document.getElementById("publishBtn");
-const listBtn = document.getElementById("listBtn");
+const partlistBtn = document.getElementById("partlistBtn");
+const subscribeBtn = document.getElementById("subscribeBtn");
 
-// 인풋 객체
+// Input DOM
+let sessionInput = document.getElementById("session");
 let subject = document.getElementById("subject");
 let password = document.getElementById("password");
+let nickname = document.getElementById("name");
 
+// Varibale
+/**
+ * @description 옴니톡 생성자 함수
+ */
 let omnitalk;
-let list;
+let partRoomId;
+let roomList;
+let sessionId;
+let pubIdx;
+let partlist;
+
+/**
+ * @description 세션 생성 함수
+ * 1. 세션을 만들기전 웹소켓 연결을 진행한다.
+ * 2. 웹 소켓 객체(wss)가 이미 존재한다면 그대로 반환
+ * 3. 웹 소켓 객체가 존재하지 않는다면 웹 소켓 생성 후 이벤트 수신
+ */
+async function getSession() {
+  sessionId = await omnitalk.createSession();
+  console.log(sessionId);
+  sessionInput.value = sessionId;
+}
+
+/**
+ *@description 방 생성 함수
+ */
+async function makeRoom() {
+  let opt = {
+    subject: subject.value || "Untitled",
+    secret: password.value,
+    room_type: "videoroom",
+    start_date: getUnixTimestamp(),
+    end_date: getUnixTimestamp() + 60 * 60,
+  };
+
+  roomId = await omnitalk.createRoom(sessionId, opt);
+}
 
 /**
  * 룸 리스트 요청 함수
  */
 async function getRoomList() {
-  /**
-   * 옴니톡 객체 생성
-   *@param {string} serviceId - 옴니톡에서 발급 받은 서비스 아이디
-   * 객체를 생성하면서 roomId, sessionId를 초기화
-   */
-  omnitalk = new Omnitalk("AMNL-VPMG-VVD1-ZCGS");
+  // sessionId - createSession()을 통해 만든 세션 아이디
+  roomList = await omnitalk.roomList(sessionId);
+  console.log("roomList");
+  console.log(roomList);
 
-  /**
-   * 웹 소켓 객체(wss)가 이미 존재한다면 그대로 반환
-   * 웹 소켓 객체가 존재하지 않는다면 웹 소켓 생성 후 이벤트 수신
-   */
-  sessionId = await omnitalk.createSession();
-  console.log("sessionId", sessionId);
+  const roomNode = document.getElementById("roomList");
+  roomList.map((item) => {
+    console.log("roomItem");
+    console.log(item);
 
-  /**
-   * 방 리스트 요청
-   *@param {string} sessionId - createSession()을 통해 만든 세션 아이디
-   *@
-   */
-  list = await omnitalk.roomList(sessionId);
+    const room = document.createElement("div");
+    const joinRoomBtn = document.createElement("button");
+    const title = document.createElement("div");
+    const content = document.createElement("div");
 
-  console.log("list");
-  console.log(list);
+    title.setAttribute("class", "title");
+    title.innerHTML = item.subject || "제목 없음";
+
+    content.setAttribute("class", "content");
+
+    joinRoomBtn.innerHTML = "참석";
+    joinRoomBtn.setAttribute("class", "smallBtn");
+    joinRoomBtn.onclick = function () {
+      joinRoom(item.room_id);
+    };
+
+    room.setAttribute("class", "room");
+
+    content.appendChild(joinRoomBtn);
+    room.appendChild(title);
+    room.appendChild(content);
+    roomNode.appendChild(room);
+  });
+}
+
+/**
+ * @event 방 참여 요청 이벤트
+ */
+async function joinRoom(roomId) {
+  partRoomId = roomId;
+  console.log("pubIdx");
+  console.log(pubIdx);
+  let opt = {
+    secret: undefined,
+    user_id: undefined,
+    user_name: nickname.value,
+  };
+
+  result = await omnitalk.joinRoom(sessionId, roomId, opt);
+}
+
+async function handleSubscribe(pubIdx) {
+  await omnitalk.subscribe(pubIdx);
 }
 
 window.onload = function () {
-  getRoomList();
+  // serviceId - 옴니톡에서 발급 받은 서비스 아이디 / 객체를 생성하면서 roomId, sessionId를 초기화
+  omnitalk = new Omnitalk("AMNL-VPMG-VVD1-ZCGS");
 
-  createRoomBtn.onclick = async () => {
-    console.log(subject.value);
-    console.log(password.value);
-    let opt = {
-      subject: subject.value,
-      secret: password.value,
-      room_type: "videoroom",
-      start_date: getUnixTimestamp(),
-      end_date: getUnixTimestamp() + 60 * 60,
-    };
-
-    roomId = await omnitalk.createRoom(sessionId, opt);
+  createSessionBtn.onclick = () => {
+    getSession();
   };
 
   createRoomBtn.onclick = async () => {
-    console.log(subject.value);
-    console.log(password.value);
+    makeRoom();
+  };
+
+  roomListBtn.onclick = async () => {
+    getRoomList();
+  };
+
+  publishBtn.onclick = async () => {
     let opt = {
-      subject: subject.value,
-      secret: password.value,
-      room_type: "videoroom",
-      start_date: getUnixTimestamp(),
-      end_date: getUnixTimestamp() + 60 * 60,
+      audio: true,
+      video: true,
+      recording: true,
     };
 
-    roomId = await omnitalk.createRoom(sessionId, opt);
+    pubIdx = await omnitalk.publish(opt);
+  };
+
+  partlistBtn.onclick = async () => {
+    partlist = await omnitalk.partiList(partRoomId);
+    console.log(partlist);
+
+    const roomNode = document.getElementById("partiList");
+    partlist.map((item) => {
+      console.log("여기");
+      console.log(item);
+      const room = document.createElement("div");
+      const joinButton = document.createElement("button");
+      joinButton.innerHTML = "구독하기";
+      room.setAttribute("class", "room");
+      joinButton.setAttribute("class", "smallBtn");
+      room.innerHTML = item.publish_idx || "제목 없음";
+      joinButton.onclick = function () {
+        handleSubscribe(item.publish_idx);
+      };
+      roomNode.appendChild(room);
+      room.appendChild(joinButton);
+    });
+  };
+
+  subscribeBtn.onclick = async () => {
+    console.log("----------------------------subscribeBtn");
+    console.log(pubIdx);
+    console.log(partlist);
+    await omnitalk.subscribe(pubIdx);
   };
 };
-
-//getRoomList();
